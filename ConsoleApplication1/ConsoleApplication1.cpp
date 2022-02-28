a1c8417fd5e44e93bda9dcc91783cc67a21ba6d0
Вадим Дергачев <vadim.valentinovic@mail.ru>
2022-02-28 21:47:41 +0300

    Убрал cin, добавил кейс 6

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <ctime>
 void summa()
 {
 	int a, b, c;
-	std::cin >> a;
-	std::cin >> b;
+	scanf_s("%d", &a);
+	scanf_s("%d", &b);
 	printf("\nC=%d\n", a + b);
 
 }
@@ -15,8 +17,8 @@ void summa()
 int main()
 {
 	int p;
-	printf("What we want to do?\n 1-summ;\n 2-sub; \n 3-multip; \n 4-div; \n 5-exp\n");
-	std::cin >> p;
+	printf("What we want to do?\n 1-summ;\n 2-sub; \n 3-multip; \n 4-div; \n 5-exp; \n 6-summ_matrix;\n");
+	scanf_s("%d", &p);
 	switch (p)
 	{
 	case 1:
@@ -29,24 +31,24 @@ int main()
 	case 2:
 	{
 		int a, b, c;
-		std::cin >> a;
-		std::cin >> b;
+		scanf_s("%d", &a);
+		scanf_s("%d", &b);
 		printf("\nC=%d\n", a - b);
 		break;
 	}
 	case 3:
 	{
 		int a, b, c;
-		std::cin >> a;
-		std::cin >> b;
+		scanf_s("%d", &a);
+		scanf_s("%d", &b);
 		printf("\nC=%d\n", a * b);
 		break;
 	}
 	case 4:
 	{
 		int a, b, c;
-		std::cin>>a;
-		std::cin >> b;
+		scanf_s("%d", &a);
+		scanf_s("%d", &b);
 		printf("\nC=%d\n", a / b);
 		break;
 	}
@@ -59,6 +61,34 @@ int main()
 		c = pow(a, b);
 		printf("\nC=%d\n", c);
 		break;
+	}
+	case 6:
+	{
+		srand(time(NULL));
+		const int N = 3;
+		int a[N][N];
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				a[i][j] = rand() % 100;
+			}
+		}
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				printf("%d ", a[i][j]);
+			}
+			printf("\n");
+		}
+			
+		
+		break;
+	}
+	default:
+	{
+	
 	}
 	}
 	return 0;
